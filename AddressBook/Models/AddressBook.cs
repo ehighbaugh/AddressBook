41498408e6c060058ee9c51bb452f18c91678a80
Erin Highbaugh <erinhighbaugh@gmail.com>
2018-05-10 12:16:34 -0400

    Initialized project; created AddressBook and Contact classes

---
+++ AddressBook/Models/AddressBook.cs
@@ -0,0 +1,12 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+
+namespace AddressBook.Models
+{
+    public class AddressBook
+    {
+        public virtual List<Contact> Contacts { get; set; }
+    }
+}
---
+++ AddressBook/Models/Contact.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+
+namespace AddressBook.Models
+{
+    public class Contact
+    {
+        public int ContactId { get; set; }
+        public string FirstName { get; set; }
+        public string LastName { get; set; }
+        public string FullName => FirstName + " " + LastName;
+        public string Email { get; set; }
+        public string Mobile { get; set; }
+        public string Landline { get; set; }
+        public string Notes { get; set; }
+    }
+}
